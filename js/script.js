9930284299f5bb8f8dcfc9ac3ef3f30e92f3b342
dianedouglas & Lee Kebler <student@epicodus.com>
2014-07-14 10:35:50 -0700

    Passes third BDD test where it checks point value in 2 arrays.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var scrabbleWord = function(word){
   var onePointLetters = ["a","e","i","o","u","l","n","r","s","t"];
+  var twoPointLetters = ["d", "g"];
   var score = 0;
 
   for(var i = 0; i < word.length; i++){
     if (onePointLetters.indexOf(word.charAt(i)) >= 0){
       score++;
-    };
-  };
+    } else if (twoPointLetters.indexOf(word.charAt(i)) >= 0){
+      score += 2;
+    }
+  }
   return score;
 }
--- specs/specs.js
+++ specs/specs.js
@@ -5,5 +5,8 @@ describe("scrabbleWord",function(){
   it("it takes 2 of the same letters and returns their total value.", function(){
     scrabbleWord("aa").should.equal(2);
   });
+  it("it takes a letter from a different point value and returns its score",function(){
+    scrabbleWord("d").should.equal(2);
+  });
 });
 
