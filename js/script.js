0ae0a4442bc47d6d65840e300b0b7438e54229ab
dianedouglas & Lee Kebler <student@epicodus.com>
2014-07-14 12:35:30 -0700

    Refactors code by creating pointLookup object to store arrays of letters with their point value as a key.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
 var scrabbleWord = function(word){
-  var onePointLetters = ["a","e","i","o","u","l","n","r","s","t"];
-  var twoPointLetters = ["d", "g"];
-  var threePointLetters = ["b","c","m","p"];
-  var fourPointLetters = ["f","h","v","w","y"];
-  var fivePointLetters = ["k"];
-  var eightPointLetters = ["j","x"];
-  var tenPointLetters = ["q","z"];
+
+  var pointLookup = {
+  1 : ["a","e","i","o","u","l","n","r","s","t"],
+  2 : ["d", "g"],
+  3 : ["b","c","m","p"],
+  4 : ["f","h","v","w","y"],
+  5 : ["k"],
+  8 : ["j","x"],
+  10 : ["q","z"]
+  }
+
   var score = 0;
 
-  for(var i = 0; i < word.length; i++){
-    if (onePointLetters.indexOf(word.charAt(i)) >= 0){
-      score++;
-    } else if (twoPointLetters.indexOf(word.charAt(i)) >= 0){
-      score += 2;
-    } else if (threePointLetters.indexOf(word.charAt(i)) >= 0){
-      score += 3;
-    } else if (fourPointLetters.indexOf(word.charAt(i)) >= 0){
-      score += 4;
-    } else if (fivePointLetters.indexOf(word.charAt(i)) >= 0){
-      score += 5;
-    } else if (eightPointLetters.indexOf(word.charAt(i)) >= 0){
-      score += 8;
-    } else if (tenPointLetters.indexOf(word.charAt(i)) >= 0){
-      score += 10;
-    } else { alert("you've obviously never played this before and are trying to trick me!");
-      score = 0;
-      break;
-    };
-  };
+  for ( var letterArray in pointLookup){
+    for(var i = 0; i < word.length; i++){
+      if(pointLookup[letterArray].indexOf(word.charAt(i)) >=0){
+        score = score + parseInt(letterArray);
+      }
+    }
+  }
+
   return score;
 };
 
