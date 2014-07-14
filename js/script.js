d9cbd793fef3a2e01646774f8839856363724f5e
dianedouglas & Lee Kebler <student@epicodus.com>
2014-07-14 10:29:38 -0700

    passed first two BDD tests

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 var scrabbleWord = function(word){
-  return 1;
+  var onePointLetters = ["a","e","i","o","u","l","n","r","s","t"];
+  var score = 0;
+
+  for(var i = 0; i < word.length; i++){
+    if (onePointLetters.indexOf(word.charAt(i)) >= 0){
+      score++;
+    };
+  };
+  return score;
 }
